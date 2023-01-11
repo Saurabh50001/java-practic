31558485b9c36ea41bd2c2c54462cac5f1dc72d2
Saurabh kumar <sks201998@gmail.com>
2023-01-11 06:40:21 +0530

    java practic

---
+++ about.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class about {
+    public static void main(String[] args){
+        int num;
+        System.out.println("Enter the value : ");
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        System.out.println("Entered integer value : " + num);
+
+    }
+}
